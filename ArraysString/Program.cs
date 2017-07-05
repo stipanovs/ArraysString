5964a8143b287839630ff1c5ccacac70891c29e3
stipanovs <sergiu.stipanov@amdaris.com>
2017-07-05 10:23:36 +0300

    Add project files.

---
+++ ArraysString/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ArraysString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] arr = {55, 0, 8, 9, 11, 45, 40, 13};
+            //int[] arr = { 5, 8, 9, 9, 9, 10, 11, 40, 43 };
+            //DeleteEven(ref arr);
+            //InsertElemBeginDigit(ref arr, 777, 1);
+            //DeleteRepeatElem(ref arr);
+            //InsertElemPairs(ref arr, 5555);
+            //DeleteZeroElem(ref arr);
+            
+            //PrintArray(arr);
+            int[,] arr2Dimens = new int[,] { { 4, 1 }, { 6, 9 }, {5, 1} };
+            TwoDimArr arr2D = new TwoDimArr();
+            arr2D.PrintArr(arr2Dimens);
+
+
+            Console.ReadKey();
+        }
+        
+
+        public static void PrintArray(int[] arr)
+        {
+            foreach (int i in arr)
+            {
+                Write(i + ", ");
+            }
+        }
+
+        // 1
+        public static void DeleteEven(ref int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    for (int a = i; a < arr.Length-1; a++)
+                    {
+                        arr[a] = arr[a + 1];
+                    }
+                    Array.Resize(ref arr, arr.Length-1);
+                }
+            }
+        }
+
+        // 2
+        public static void InsertElemBeginDigit(ref int[] arr, int elem, int digit)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].ToString()[0] == digit.ToString()[0])
+                {
+                    Array.Resize(ref arr, arr.Length + 1);
+                    
+                    for (int j = arr.Length-1 ; j > i+1; j--)
+                    {
+                        arr[j] = arr[j-1];
+
+                    }
+                    arr[i+1] = elem;
+                }
+            }
+        }
+
+        // 3
+        public static void DeleteRepeatElem(ref int[] arr)
+        {
+            for (int j = 0; j < arr.Length; j++)
+            {
+                int find = arr[j];
+                bool first = false;
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] == find & !first)
+                    {
+                        first = true;
+                    }
+                    else if (arr[i] == find & first)
+                    {
+                        for (int a = i; a < arr.Length - 1; a++)
+                        {
+                            arr[a] = arr[a + 1];
+                        }
+                        Array.Resize(ref arr, arr.Length - 1);
+                        i--;
+                    }
+                }
+            }
+        }
+
+        // 4 
+        public static void InsertElemPairs(ref int[] arr, int elem)
+        {
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                if (Math.Abs(arr[i]) == Math.Abs(arr[i+1]))
+                {
+                    Array.Resize(ref arr, arr.Length + 1);
+
+                    for (int j = arr.Length - 1; j > i + 1; j--)
+                    {
+                        arr[j] = arr[j - 1];
+                    }
+                    arr[i + 1] = elem;
+                }
+            }
+        }
+
+        // 5
+
+        public static void DeleteZeroElem(ref int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 0)
+                {
+                    for (int a = i; a < arr.Length - 1; a++)
+                    {
+                        arr[a] = arr[a + 1];
+                    }
+                    Array.Resize(ref arr, arr.Length - 1);
+                }
+            }
+        }
+
+
+
+    }
+}
---
+++ ArraysString/TwoDimArr.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysString
+{
+    class TwoDimArr
+    {
+        public void PrintArr(int[,] arr)
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write(arr[i,j] + ", ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public void InsertLineAfterMinElem(int[,] arr)
+        {
+            int min = arr[0, 0];
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if (arr[i, j] < min)
+                    {
+                        
+                    }
+                }
+               
+            }
+        }
+    }
+}
